80d039c64804fd77fa8d0aaec081e5ebcc50c72a
Neil Burningham <neil.burningham@mediakind.com>
2022-06-28 17:41:53 +0100

    New emacs config...

--- init.el
+++ init.el
@@ -1,255 +1,109 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; Always install things we try to configure
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;; Vertico & friends
+(use-package vertico
+  :init
+  (vertico-mode)
+
+  ;; Different scroll margin
+  ;; (setq vertico-scroll-margin 0)
+
+  ;; Show more candidates
+  ;; (setq vertico-count 20)
+
+  ;; Grow and shrink the Vertico minibuffer
+  ;; (setq vertico-resize t)
+
+  ;; Optionally enable cycling for `vertico-next' and `vertico-previous'.
+  ;; (setq vertico-cycle t)
+  )
+
+;; Persist history over Emacs restarts. Vertico sorts by history position.
+(use-package savehist
+  :init
+  (savehist-mode))
+
+;; Use the `orderless' completion style.
+(use-package orderless
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+;; Enable richer annotations using the Marginalia package
+(use-package marginalia
+  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
+  :bind (("M-A" . marginalia-cycle)
+         :map minibuffer-local-map
+         ("M-A" . marginalia-cycle))
+
+  ;; The :init configuration is always executed (Not lazy!)
+  :init
+
+  ;; Must be in the :init section of use-package such that the mode gets
+  ;; enabled right away. Note that this forces loading the package.
+  (marginalia-mode))
+
+;; A few more useful configurations...
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  ;; (setq read-extended-command-predicate
+  ;;       #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+(menu-bar-mode -1)
+;; (scroll-bar-mode -1)
+(tool-bar-mode -1)
+;; (setq visible-bell t)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-for-comint-mode t)
- '(apropos-do-all t)
- '(c-basic-offset 4)
- '(c-offsets-alist
-   (quote
-    ((brace-list-open . 0)
-     (substatement-open . 0)
-     (extern-lang-open . 0)
-     (extern-lang-close . 0)
-     (inextern-lang . 0)
-     (innamespace . 0))))
- '(column-number-mode t)
- '(custom-buffer-indent 4)
- '(desktop-base-file-name "desktop")
- '(desktop-save (quote ask-if-new))
- '(desktop-save-mode t)
- '(ecb-compilation-buffer-names
-   (quote
-    (("*Calculator*")
-     ("*vc*")
-     ("*vc-diff*")
-     ("*Apropos*")
-     ("*Occur*")
-     ("*shell*")
-     ("\\*[cC]ompilation.*\\*" . t)
-     ("\\*i?grep.*\\*" . t)
-     ("*JDEE Compile Server*")
-     ("*Help*")
-     ("*Completions*")
-     ("*Backtrace*")
-     ("*Compile-log*")
-     ("*bsh*")
-     ("*Messages*")
-     ("\\*Symref.*" . t))))
- '(ecb-compile-window-height 6)
- '(ecb-layout-name "left11")
- '(ecb-layout-window-sizes
-   (quote
-    (("left8"
-      (ecb-directories-buffer-name 0.32905982905982906 . 0.2318840579710145)
-      (ecb-sources-buffer-name 0.32905982905982906 . 0.21739130434782608)
-      (ecb-methods-buffer-name 0.32905982905982906 . 0.3333333333333333)
-      (ecb-history-buffer-name 0.32905982905982906 . 0.2028985507246377)))))
- '(ecb-options-version "2.40")
- '(ecb-tip-of-the-day nil)
- '(ecb-toggle-layout-sequence (quote ("left2" "left11")))
- '(gdb-many-windows t)
- '(global-auto-revert-mode nil)
- '(grep-find-ignored-directories
-   (quote
-    ("SCCS" "RCS" "CVS" "MCVS" ".svn" ".git" ".hg" ".bzr" "_MTN" "_darcs" "{arch}" "output")))
- '(grep-highlight-matches t)
- '(helm-buffer-max-length nil)
- '(indent-tabs-mode nil)
- '(indicate-buffer-boundaries (quote ((top . left) (bottom . right))))
- '(indicate-empty-lines t)
- '(mouse-yank-at-point t)
- '(normal-erase-is-backspace t)
- '(nxml-attribute-indent 4)
- '(nxml-child-indent 4)
- '(nxml-outline-child-indent 4)
- '(package-selected-packages
-   (quote
-    (typescript-mode yaml-mode csharp-mode markdown-mode groovy-mode lua-mode popwin nyan-mode helm-swoop helm-pydoc helm-projectile helm-gtags go-mode gitignore-mode function-args es-windows es-lib dummy-h-mode auctex)))
- '(require-final-newline t)
- '(save-interprogram-paste-before-kill t)
- '(save-place-mode t nil (saveplace))
- '(scroll-bar-mode nil)
- '(select-enable-clipboard t)
- '(select-enable-primary t)
- '(semantic-default-submodes
-   (quote
-    (global-semantic-highlight-func-mode global-semantic-decoration-mode global-semantic-stickyfunc-mode global-semantic-idle-completions-mode global-semantic-idle-scheduler-mode global-semanticdb-minor-mode global-semantic-idle-summary-mode)))
- '(semantic-mode t)
- '(server-mode t)
- '(sgml-basic-offset 4)
- '(show-paren-mode t)
- '(show-trailing-whitespace t)
- '(size-indication-mode t)
- '(tool-bar-mode nil)
- '(uniquify-buffer-name-style (quote forward) nil (uniquify))
- '(use-dialog-box nil)
- '(vc-handled-backends nil)
- '(visible-bell t))
+ '(package-selected-packages '(vertico use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 83 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
-
-;; MELPA Stuff
-(require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-
-(when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
-
-;; Create my E/// style
-(defconst my-c-style
-  '('gnu
-    (c-basic-offset . 4)
-    ;; (c-tab-always-indent . nil)
-    (c-offsets-alist . ((brace-list-open . 0)
-                        (substatement-open . 0)
-                        (innamespace . 0)))
-    (c-backslash-column . 1)
-    (c-backslash-max-column . 120)
-    (c-auto-align-backslashes . t)
-    )
-  "E/// C Programming Style")
-(c-add-style "E///" my-c-style)
-
-;; doxymacs integration (Ubuntu package = doxymacs)
-;;(require 'doxymacs)
-;;(add-hook 'c-mode-common-hook 'doxymacs-mode)
-;;(defun my-doxymacs-font-lock-hook ()
-;;  (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode) (eq major-mode 'java-mode))
-;;      (doxymacs-font-lock)))
-;;(add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
-
-;; Put full file name in frame title
-(setq frame-title-format
-      (list (format "%s %%S: %%j " (system-name))
-            '(buffer-file-name "%f" (dired-directory dired-directory "%b"))))
-
-;; Make .h files always open as C++
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-;; Window freezing mods
-(defadvice pop-to-buffer (before cancel-other-window first)
-  (ad-set-arg 1 nil))
-
-(ad-activate 'pop-to-buffer)
-
-;; Toggle window dedication, bind to pause
-(defun toggle-window-dedicated ()
-  "Toggle whether the current active window is dedicated or not"
-  (interactive)
-  (message
-   (if (let (window (get-buffer-window (current-buffer)))
-         (set-window-dedicated-p window
-                                 (not (window-dedicated-p window))))
-       "Window '%s' is dedicated"
-     "Window '%s' is normal")
-   (current-buffer)))
-(global-set-key [pause] 'toggle-window-dedicated)
-
-;; Stuff I want, nicked from better-defaults
-(autoload 'zap-up-to-char "misc"
-  "Kill up to, but not including ARGth occurrence of CHAR." t)
-
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'forward)
-
-(require 'saveplace)
-(setq-default save-place t)
-
-(global-set-key (kbd "M-/") 'hippie-expand)
-;;(global-set-key (kbd "C-x C-b") 'ibuffer)
-(global-set-key (kbd "M-z") 'zap-up-to-char)
-
-;;(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-;;(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-;;(global-set-key (kbd "C-M-s") 'isearch-forward)
-;;(global-set-key (kbd "C-M-r") 'isearch-backward)
-
-(setq load-prefer-newer t)
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
-(setq save-place-file (concat user-emacs-directory "places"))
-(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups"))))
-
-;; Helm
-(require 'helm)
-(require 'helm-config)
-
-(global-set-key (kbd "C-x b")   #'helm-mini)
-(global-set-key (kbd "C-x C-b") #'helm-buffers-list)
-(global-set-key (kbd "M-x")     #'helm-M-x)
-(global-set-key (kbd "C-x C-f") #'helm-find-files)
-(global-set-key (kbd "C-x C-r") #'helm-recentf)
-(global-set-key (kbd "C-x r l") #'helm-filtered-bookmarks)
-(global-set-key (kbd "M-y")     #'helm-show-kill-ring)
-(global-set-key (kbd "M-s o")   #'helm-swoop)
-(global-set-key (kbd "M-s /")   #'helm-multi-swoop)
-(global-set-key (kbd "C-c <SPC>") #'helm-all-mark-rings)
-
-(helm-mode t)
-
-;; Projectile
-(projectile-global-mode)
-(setq projectile-completion-system 'helm)
-(helm-projectile-on)
-(setq projectile-enable-caching t)
-
-;; Helm Gtags
-(setq
- helm-gtags-ignore-case t
- helm-gtags-auto-update t
- helm-gtags-use-input-at-cursor t
- helm-gtags-pulse-at-cursor t
- helm-gtags-prefix-key "\C-cg"
- helm-gtags-suggested-key-mapping nil
  )
-
-(require 'helm-gtags)
-(add-hook 'dired-mode-hook 'helm-gtags-mode)
-(add-hook 'eshell-mode-hook 'helm-gtags-mode)
-(add-hook 'c-mode-hook 'helm-gtags-mode)
-(add-hook 'c++-mode-hook 'helm-gtags-mode)
-(add-hook 'asm-mode-hook 'helm-gtags-mode)
-
-(define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
-(define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
-(define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
-(define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
-(define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
-(define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-
-;; Helm ripgrep integration
-(setq helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
-(setq helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
-
-;; Magit
-;;(setq magit-last-seen-setup-instructions "1.4.0")
-;;(global-set-key (kbd "C-x g") 'magit-status)
-
-;; Nyan
-;; (nyan-mode t)
-
-;; Needed for ECB
-(setq stack-trace-on-error t)
-
-;; Markdown mode (from emacs-goodies-el)
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; Key bindings
-(global-set-key (kbd "C-x r c") 'clear-rectangle)
-
-;; Enable disabled features
-(put 'downcase-region 'disabled nil)
-(put 'upcase-region 'disabled nil)
-(put 'dired-find-alternate-file 'disabled nil)
